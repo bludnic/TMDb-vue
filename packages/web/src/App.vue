58848db9feaf13e8473770a86b2bbd83b61134a6
bludnic <victorcpla@gmail.com>
2019-10-07 23:44:55 +0300

    feat: add AppNavigationDrawer

--- packages/web/src/App.vue
+++ packages/web/src/App.vue
@@ -1,29 +1,34 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="app">
+    <app-navigation-drawer :items="genresMock"/>
+
+    <v-content>
+      <v-container
+        class="fill-height"
+        fluid
+      >
+        <router-view/>
+      </v-container>
+    </v-content>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script>
+import AppNavigationDrawer from '@/components/AppNavigationDrawer'
+
+export default {
+  data: () => ({
+    genresMock: [
+      { title: 'Dashboard', icon: 'mdi-view-dashboard' },
+      { title: 'Photos', icon: 'mdi-image' },
+      { title: 'About', icon: 'mdi-help-box' }
+    ]
+  }),
+  components: {
+    AppNavigationDrawer
   }
 }
+</script>
+
+<style lang="scss">
 </style>
---
+++ packages/web/src/components/AppNavigationDrawer.vue
@@ -0,0 +1,41 @@
+<template>
+  <v-navigation-drawer
+    permanent
+    app
+  >
+    <v-card flat class="pa-3">
+      <h1>TMDb Vue App</h1>
+    </v-card>
+
+    <v-divider></v-divider>
+
+    <v-list>
+      <v-list-tile
+        v-for="item in items"
+        :key="item.title"
+        @click=""
+      >
+        <v-list-tile-content>
+          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+        </v-list-tile-content>
+
+        <v-list-tile-action>
+          <v-icon>{{ item.action }}</v-icon>
+        </v-list-tile-action>
+      </v-list-tile>
+    </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data: () => ({
+  }),
+  props: {
+    items: {
+      type: Array,
+      required: true
+    }
+  }
+}
+</script>
