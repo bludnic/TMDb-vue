667b27c0c882ee4bce294aea3556d4e9c574cc43
bludnic <victorcpla@gmail.com>
2019-10-09 19:43:22 +0300

    fix(Movie): wait until isFulfilled

--- packages/web/src/views/Movie.vue
+++ packages/web/src/views/Movie.vue
@@ -2,50 +2,41 @@
   <v-container grid-list-md>
     <h1>{{ movie.title }}</h1>
 
-    <v-layout row wrap>
-      <v-flex md4>
-        <v-img
-          :src="`${TMDbURL}/${movie.poster_path}`"
-          aspect-ratio="0.66"
-        />
+    <template v-if="isFulfilled">
+      <v-layout row wrap>
+        <v-flex md4>
+          <v-img
+            :src="`${TMDbURL}/${movie.poster_path}`"
+            aspect-ratio="0.66"
+          />
 
-      </v-flex>
+        </v-flex>
 
-      <v-flex md8>
-        <movie-meta
-          :title="movie.title"
-          :overview="movie.overview"
-          :releaseDate="movie.release_date"
-          :productionCountries="movie.production_countries"
-          :tagline="movie.tagline"
-          :budget="movie.budget"
-          :revenue="movie.revenue"
-          :backdropPath="movie.backdrop_path"
-          :posterPath="movie.poster_path"
-          :runtime="movie.runtime"
-          :adult="movie.adult"
-        />
+        <v-flex md8>
+          <movie-meta
+            :title="movie.title"
+            :overview="movie.overview"
+            :releaseDate="movie.release_date"
+            :productionCountries="movie.production_countries"
+            :tagline="movie.tagline"
+            :budget="movie.budget"
+            :revenue="movie.revenue"
+            :backdropPath="movie.backdrop_path"
+            :posterPath="movie.poster_path"
+            :runtime="movie.runtime"
+            :adult="movie.adult"
+          />
+        </v-flex>
+      </v-layout>
 
-        <v-list class="transparent">
-          <v-list-tile>year: {{ movie.release_date }}</v-list-tile>
-          <v-list-tile>countries {{ movie.production_countries }}</v-list-tile>
-          <v-list-tile>slogan {{ movie.tagline }}</v-list-tile>
-          <v-list-tile>budget {{ movie.budget }}</v-list-tile>
-          <v-list-tile>revenue {{ movie.revenue }}</v-list-tile>
+      <v-layout>
 
-          <v-list-tile>movie.backdrop_path</v-list-tile>
-        </v-list>
-      </v-flex>
-    </v-layout>
+        <v-flex xs12>
+          <div class="subheading">{{ movie.overview }}</div>
+        </v-flex>
 
-    <v-layout>
-
-      <v-flex xs12>
-        <div class="subheading">{{ movie.overview }}</div>
-      </v-flex>
-
-    </v-layout>
-
+      </v-layout>
+    </template>
   </v-container>
 </template>
 
@@ -62,12 +53,14 @@ export default Vue.extend({
   },
   data: () => ({
     movie: {},
-    TMDbURL: process.env.VUE_APP_TMDB_CDN
+    TMDbURL: process.env.VUE_APP_TMDB_CDN,
+    isFulfilled: false
   }),
   methods: {
     async fetchMovie () {
       this.movie = await apiClient.get(`movie/${this.id}`)
         .then(res => res.data)
+      this.isFulfilled = true
     }
   },
   components: {
@@ -75,7 +68,7 @@ export default Vue.extend({
   },
   props: {
     id: {
-      type: String,
+      type: Number,
       required: true
     }
   }
