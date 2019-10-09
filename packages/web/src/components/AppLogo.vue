37dc3f96710ade014aba57fb3bffbc0a831c551c
bludnic <victorcpla@gmail.com>
2019-10-09 18:29:55 +0300

    feat(client): add AppLogo component

---
+++ packages/web/src/components/AppLogo.vue
@@ -0,0 +1,28 @@
+<template>
+  <v-card
+    @click="goToHome"
+    class="logo"
+    flat
+  >
+    <h1>TMDb Vue App</h1>
+  </v-card>
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  name: 'app-logo',
+  methods: {
+    goToHome () {
+      this.$router.push('/')
+    }
+  }
+})
+</script>
+
+<style lang="sass">
+  .logo
+    padding: 16px
+    cursor: pointer
+</style>
