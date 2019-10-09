9561ecb02f051ee58523daaec8ac85985a2250d4
bludnic <victorcpla@gmail.com>
2019-10-09 18:31:49 +0300

    feat(AppNavigationDrawer): AppLogo & goToCategory method

--- packages/web/src/components/AppNavigationDrawer.vue
+++ packages/web/src/components/AppNavigationDrawer.vue
@@ -3,39 +3,53 @@
     permanent
     app
   >
-    <v-card flat class="pa-3">
-      <h1>TMDb Vue App</h1>
-    </v-card>
+    <app-logo/>
 
     <v-divider></v-divider>
 
     <v-list>
+
       <v-list-tile
         v-for="item in items"
-        :key="item.title"
-        @click=""
+        :key="item.id"
+        active-class="primary--text"
+        :class="item.id === $route.params.id ? 'primary--text' : ''"
+        @click="goToCategory(item.id)"
       >
         <v-list-tile-content>
-          <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+          <v-list-tile-title>{{ item.name }}</v-list-tile-title>
         </v-list-tile-content>
-
-        <v-list-tile-action>
-          <v-icon>{{ item.action }}</v-icon>
-        </v-list-tile-action>
       </v-list-tile>
     </v-list>
   </v-navigation-drawer>
 </template>
 
-<script>
-export default {
+<script lang="ts">
+import Vue from 'vue'
+
+import AppLogo from './AppLogo'
+
+export default Vue.extend({
   data: () => ({
   }),
+  methods: {
+    goToCategory (id: number) {
+      this.$router.replace({
+        name: 'genre',
+        params: {
+          id
+        }
+      })
+    }
+  },
+  components: {
+    AppLogo
+  },
   props: {
     items: {
       type: Array,
       required: true
     }
   }
-}
+})
 </script>
